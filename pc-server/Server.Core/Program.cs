e8efbb54bd9b0656c029187918fb51869ea31589
FrancisWong924 <franciswong924@gmail.com>
2025-11-07 14:52:52 +0800

    feat: init .NET 9.0 PC server (ready for Bluetooth)

---
+++ pc-server/Server.Core/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Exercise Controller PC Server v1.0");
+Console.WriteLine("Waiting for Android via Bluetooth...");
+Console.WriteLine("Press Ctrl+C to exit.");
+while (true) { await Task.Delay(100); }
